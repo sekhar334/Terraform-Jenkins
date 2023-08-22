f13a518f7f0f3d9271d979eb603bd8355484d4f2
sekhar334 <137845373+sekhar334@users.noreply.github.com>
2023-08-22 16:32:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = " ami-08a52ddb321b32a8c" # us-east-1
+  ami           = "ami-08a52ddb321b32a8c" # us-east-1
   instance_type = "t2.medium"
   tags = {
       Name = "Terraform-Instance"
